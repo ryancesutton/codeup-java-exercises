f18baa714c2451857304d121d06b3bc7f08982b0
Ry Sutton <ryancesutton@gmail.com>
2020-05-01 12:25:08 -0500

    complete three word prompt

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -6,9 +6,18 @@ public class ConsoleExercises {
         double pi = 3.14159;
         System.out.printf("The value of pi is approximately %.2f", pi);
         System.out.println("");
+
         Scanner scanner = new Scanner(System.in);
+
         System.out.print("Enter an integer...");
         int userInput = scanner.nextInt();
         System.out.println("You entered: " + userInput);
+
+        System.out.println("Enter three words...");
+        String first = scanner.next();
+        String second = scanner.next();
+        String third = scanner.next();
+        System.out.printf("You entered: %s %s %s", first, second, third);
+
     }
 }
