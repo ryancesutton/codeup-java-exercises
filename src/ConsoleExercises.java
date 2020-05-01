627bf1ed4845682c2aef335f5c8c3cbec5f917ca
Ry Sutton <ryancesutton@gmail.com>
2020-05-01 12:18:20 -0500

    complete prompting for integer

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class ConsoleExercises {
     public static void main(String args[]){
+
         double pi = 3.14159;
         System.out.printf("The value of pi is approximately %.2f", pi);
+        System.out.println("");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter an integer...");
+        int userInput = scanner.nextInt();
+        System.out.println("You entered: " + userInput);
     }
 }
