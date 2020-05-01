d49a52ec2c32d74daedfa2888fa5c76654cc80ca
Ry Sutton <ryancesutton@gmail.com>
2020-05-01 09:33:28 -0500

    complete first five problems in exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,22 @@
 public class HelloWorld {
     public static void main(String[] args) {
+
+        int myFavoriteNumber = 10000;
+        String myString = "Ry Sutton";
+//        char myString = "Ry Sutton";
+//        String myString = 3.14159;
+
+        long myNumber = 3.14;
+
+
         System.out.println("Hello, World!");
         System.out.println("We begin java...");
+
+        System.out.println("myFavoriteNumber " + myFavoriteNumber);
+        System.out.println("myString " + myString);
+        System.out.println("myNumber " + myNumber);
+
+
 
     }
 }
