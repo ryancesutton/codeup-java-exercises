ab61e75289c0e4af690133391ac864deac596d9d
Ry Sutton <ryancesutton@gmail.com>
2020-05-04 12:31:51 -0500

    complete number three of exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ControlFlowExercises {
     public static void main(String[] args) {
 
@@ -28,17 +30,29 @@ public class ControlFlowExercises {
 //            System.out.println(i);
 //        }
 
-        for (int i = 1; i <=100; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
-                System.out.println("FizzBuzz");
-            } else if (i % 5 == 0) {
-                System.out.println("Buzz");
-            } else if (i % 3 == 0) {
-                System.out.println("Fizz");
-            } else {
-                System.out.println(i);
-            }
-        }
+//        for (int i = 1; i <=100; i++) {
+//            if (i % 3 == 0 && i % 5 == 0) {
+//                System.out.println("FizzBuzz");
+//            } else if (i % 5 == 0) {
+//                System.out.println("Buzz");
+//            } else if (i % 3 == 0) {
+//                System.out.println("Fizz");
+//            } else {
+//                System.out.println(i);
+//            }
+//        }
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter an integer: ");
+        String userInput = scanner.nextLine();
+        int userNumber = Integer.parseInt(userInput);
+
+        System.out.println(userNumber);
+        System.out.print("number | squared | cubed\n");
+        System.out.print("------ | ------- | -----\n");
+        for (int i = 1; i <= userNumber; i++) {
+            System.out.printf("%d      | %d       | %d   \n", i, i*i, (i*i*i));
+        }
     }
 }
