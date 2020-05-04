422d5a525d012e4fc530b2632055d452d2d490be
Ry Sutton <ryancesutton@gmail.com>
2020-05-04 12:00:42 -0500

    complete number one of exercise

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,44 @@
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+
+//        int i = 5;
+//
+//        while(i <= 15) {
+//            System.out.println(i);
+//            i++;
+//        }
+
+//        int i = 2;
+//
+//        do {
+//            System.out.println(i);
+//            i*=i;
+//        } while (i < 1000000);
+
+//        for (int i = 5; i <= 15; i++){
+//            System.out.println(i);
+//        }
+//
+//        for (int i = 100; i >= -10; i-=5){
+//            System.out.println(i);
+//        }
+
+
+//        for (int i = 2; i < 1000000; i*=i){
+//            System.out.println(i);
+//        }
+
+        for (int i = 1; i <=100; i++) {
+            if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+
+    }
+}
