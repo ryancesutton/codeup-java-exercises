917715f31a7b368cf3d6b0aabb63657b0c132848
Ry Sutton <ryancesutton@gmail.com>
2020-05-04 16:24:57 -0500

    add alternate solution provided by douglas

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -43,7 +43,7 @@ public class ControlFlowExercises {
 //        }
 
         Scanner scanner = new Scanner(System.in);
-//
+
 //        System.out.println("Enter an integer: ");
 //        String userInput = scanner.nextLine();
 //        int userNumber = Integer.parseInt(userInput);
@@ -55,26 +55,53 @@ public class ControlFlowExercises {
 //            System.out.printf("%d      | %d       | %d   \n", i, i*i, (i*i*i));
 //        }
 
-        System.out.println("Enter a grade 0 - 100: ");
-        String userInput = scanner.nextLine();
-        int userGrade = Integer.parseInt(userInput);
-        System.out.println("Continue? Type: [yes/no]");
-        String yesOrNo = scanner.nextLine();
-        if(yesOrNo.equals("yes")){
-            if (userGrade >= 0 && userGrade <= 59){
-                System.out.println("Your grade is: F");
-            } else if (userGrade >= 60 && userGrade <= 66){
-                System.out.println("Your grade is: D");
-            } else if (userGrade >= 67 && userGrade <= 79){
-                System.out.println("Your grade is: C");
-            } else if (userGrade >= 80 && userGrade <= 87){
-                System.out.println("Your grade is: B");
-            } else if (userGrade >= 88 && userGrade <= 100){
-                System.out.println("Your grade is: A");
+//        ALTERNATE SOLUTION
+
+        boolean userContinues = true;
+
+        do {
+            System.out.println("What number would you like to go to?");
+            int userInt = scanner.nextInt();
+            System.out.println();
+            System.out.println("Here is your table: ");
+            System.out.println();
+            System.out.println("number | squared | cubed");
+            System.out.println("------ | ------- | -----");
+            for (int i = 1; i <= userInt; i++) {
+                System.out.print(i);
+                System.out.print(" | ");
+                System.out.print(i*i);
+                System.out.print(" | ");
+                System.out.print(i*i*i);
+                System.out.println();
             }
-        } else {
-            System.out.println("You dont want to see? Ok. We're done here!");
-        }
+            System.out.print("Would you like to enter another number? [y/n] ");
+            String userResponse = scanner.next();
+            if(!userResponse.equalsIgnoreCase("y")) {
+                userContinues = false;
+            }
+        } while (userContinues);
+
+//        System.out.println("Enter a grade 0 - 100: ");
+//        String userInput = scanner.nextLine();
+//        int userGrade = Integer.parseInt(userInput);
+//        System.out.println("Continue? Type: [yes/no]");
+//        String yesOrNo = scanner.nextLine();
+//        if(yesOrNo.equals("yes")){
+//            if (userGrade >= 0 && userGrade <= 59){
+//                System.out.println("Your grade is: F");
+//            } else if (userGrade >= 60 && userGrade <= 66){
+//                System.out.println("Your grade is: D");
+//            } else if (userGrade >= 67 && userGrade <= 79){
+//                System.out.println("Your grade is: C");
+//            } else if (userGrade >= 80 && userGrade <= 87){
+//                System.out.println("Your grade is: B");
+//            } else if (userGrade >= 88 && userGrade <= 100){
+//                System.out.println("Your grade is: A");
+//            }
+//        } else {
+//            System.out.println("You dont want to see? Ok. We're done here!");
+//        }
 
 
     }
