10f93d21cb62220992ca09f65cf22877e60699b3
Ry Sutton <ryancesutton@gmail.com>
2020-05-05 10:25:56 -0500

    complete secondoutput

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -5,6 +5,9 @@ public class StringExercise {
         String brickInWall = "We don't need no education We don't need no thought control";
         System.out.println(brickInWall.substring(0, brickInWall.lastIndexOf("We")) + "\n" + brickInWall.substring(brickInWall.lastIndexOf("We")));
 
+        String checkItOut = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(checkItOut);
+
 
 
     }
