669db723562dc086af5e241ac5262139815523bb
Ry Sutton <ryancesutton@gmail.com>
2020-05-05 10:28:52 -0500

    correct first output

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -2,8 +2,8 @@ public class StringExercise {
 
     public static void main(String[] args) {
 
-        String brickInWall = "We don't need no education We don't need no thought control";
-        System.out.println(brickInWall.substring(0, brickInWall.lastIndexOf("We")) + "\n" + brickInWall.substring(brickInWall.lastIndexOf("We")));
+        String brickInWall = "We don't need no education \nWe don't need no thought control";
+        System.out.println(brickInWall);
 
         String checkItOut = "Check \"this\" out!, \"s inside of \"s!";
         System.out.println(checkItOut);
