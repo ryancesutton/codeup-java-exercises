6f24976f925789722751eddd0f2505b84b3b731c
Ry Sutton <ryancesutton@gmail.com>
2020-05-05 10:23:23 -0500

    complete first output

---
+++ src/StringExercise.java
@@ -0,0 +1,11 @@
+public class StringExercise {
+
+    public static void main(String[] args) {
+
+        String brickInWall = "We don't need no education We don't need no thought control";
+        System.out.println(brickInWall.substring(0, brickInWall.lastIndexOf("We")) + "\n" + brickInWall.substring(brickInWall.lastIndexOf("We")));
+
+
+
+    }
+}
