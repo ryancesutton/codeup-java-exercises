4fbe926f7d36377a914d43c02c7499cf34cbc526
Ry Sutton <ryancesutton@gmail.com>
2020-05-06 11:40:52 -0500

    switch integers to doubles per walkthrough

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,6 +1,12 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
 public class MethodsExercises {
 
     public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
         System.out.println("Sum: " + addItUpp(6,8));
         System.out.println("Difference: " + subtractItUp(6, 8));
         System.out.println("Product: " + multiply(6,8));
@@ -8,35 +14,37 @@ public class MethodsExercises {
         System.out.println("Modulus: " + modulus(7,4));
         System.out.println("Multiply w/ Loop: " + multiplierLoop(2, 4));
         System.out.println("Multiply w/ Rec: " + multiplyRec(10, 10));
-//        System.out.println("Get Integer: " + getInteger(0,10));
+//        System.out.println(getInteger(1,10));
+        factorial(input);
+
 
     }
 
-    public static int addItUpp(int num1, int num2){
+    public static double addItUpp(double num1, double num2){
         return num1 + num2;
     }
 
-    public static int subtractItUp(int num1, int num2){
+    public static double subtractItUp(double num1, double num2){
         return num1 - num2;
     }
 
-    public static int multiply(int num1, int num2){
+    public static double multiply(double num1, double num2){
         return num1 * num2;
     }
 
     // SHOULD RESULT IN UNDEFINED BUT GETTING INFINITY
-    public static float divide(float num1, float num2){
+    public static double divide(double num1, double num2){
         return num1 / num2;
     }
 
-    public static int modulus(int num1, int num2){
+    public static double modulus(double num1, double num2){
         return num1 / num2;
     }
 
     // MULTIPLY WITH FOR LOOP PRODUCT OF 2X4 IS IDK
-    public static int multiplierLoop(int a, int b) {
-        int result = 0;
-        for (int i = 0; i < a; i++) {
+    public static double multiplierLoop(double a, double b) {
+        double result = 0;
+        for (double i = 0; i < a; i++) {
             result += b;
         }
 
@@ -44,7 +52,7 @@ public class MethodsExercises {
     }
 
     // MULTIPLY WITH RECURSION PRODUCT OF 2X4 IS 2+3 + 1+2
-    public static int multiplyRec(int a, int b) {
+    public static double multiplyRec(double a, double b) {
 
         if ((a == 0) || (b == 0)) {
             return 0;
@@ -54,11 +62,22 @@ public class MethodsExercises {
     }
 
 //    public static int getInteger(int min, int max) {
-//        System.out.print("Enter a number between 1 and 10: ");
+//       Scanner scan = new Scanner(System.in);
+//       System.out.print("Enter a number between 1 and 10: ");
 //        int userInput = getInteger(1, 10);
-//
+//        return userInput;
 //    }
 
+    public static void factorial(Scanner scan){
+        System.out.print("Enter a number between 1 and 10: ");
+        int userInput = scan.nextInt();
+        System.out.println("You entered: " + userInput);
+        int fact = 1;
+        for(int i = 1; i <=userInput; i++){
+            fact *= i;
+        }
+        System.out.println(userInput + "! " + "= " + fact);
+    }
 
 
 
