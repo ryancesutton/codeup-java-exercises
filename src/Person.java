b4b7283fec4842e30922dddf070ce98cfc00fa13
Ry Sutton <ryancesutton@gmail.com>
2020-12-09 10:32:27 -0600

    changes in person class for helping jupiter

--- src/Person.java
+++ src/Person.java
@@ -30,23 +30,23 @@ public class Person {
 //        aPerson.setName("Rex");
 //        aPerson.sayHello();
 
-        Person person1 = new Person("John");
-        Person person2 = new Person("John");
-        System.out.println(person1.getName().equals(person2.getName()));
-        System.out.println(person1 == person2);
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+//        System.out.println(person1.getName().equals(person2.getName()));
+//        System.out.println(person1 == person2);
 
 
 //        Person person1 = new Person("John");
 //        Person person2 = person1;
 //        System.out.println(person1 == person2);
 
-//        Person person1 = new Person("John");
-//        Person person2 = person1;
-//        System.out.println(person1.getName());
-//        System.out.println(person2.getName());
-//        person2.setName("Jane");
-//        System.out.println(person1.getName());
-//        System.out.println(person2.getName());
+        Person person1 = new Person("John");
+        Person person2 = person1;
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+        person2.setName("Jane");
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
 
     }
 }
